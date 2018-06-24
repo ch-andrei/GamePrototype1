fd8e6d1d4b01c18494019e21702ca49548992628
Andrei Chubarau <andrei.chubarau@irystec.com>
2018-06-24 12:30:09 -0400

    Camera control; basic level map

---
+++ Assets/Code/Agents.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class Agent : MonoBehaviour 
+{
+    
+}
+
+
+public class AgentManager : ComponentSystem
+{
+    override
+    protected void OnUpdate()
+    {
+        
+    }
+}
